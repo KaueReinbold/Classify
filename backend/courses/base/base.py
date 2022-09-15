345b883cf4d27d3b7166a2249efded78e4d37a52
Kaue Reinbold <kaue.reinbold@outlook.com>
2022-09-15 11:56:19 -0300

    feat: add env var

--- backend/courses/base/base.py
+++ backend/courses/base/base.py
@@ -1,10 +1,14 @@
 # coding=utf-8
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+engine = create_engine(os.getenv('POSTGRES_CONNECTION_STRING'))
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
